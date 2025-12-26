3d59036f8fe60bd2e33f54f547ecf8af664d1ae0
wd <lucasalvesbispo2@gmail.com>
2025-12-26 14:52:14 +0000

    changed ftb teams sharing to default to false

--- src/main/java/com/wdiscute/starcatcher/Config.java
+++ src/main/java/com/wdiscute/starcatcher/Config.java
@@ -46,7 +46,7 @@ public class Config
     public static final ForgeConfigSpec.BooleanValue ENABLE_FTB_TEAM_SHARING = BUILDER_SERVER
             .comment("enables/disables fishes caught being unlocked for all online team members.")
             .comment("Offline players won't be awarded the entry.")
-            .define("enable_seasons", true);
+            .define("enable_seasons", false);
 
     public static final ForgeConfigSpec.BooleanValue ENABLE_SEASONS = BUILDER_SERVER
             .comment("enables/disables fishes being restricted by seasons.")
