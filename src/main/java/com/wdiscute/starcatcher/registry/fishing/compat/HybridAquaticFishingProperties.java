29ab885492eeccae3a6653ff6483c7cbc09d8efd
wd <lucasalvesbispo2@gmail.com>
2026-01-04 18:02:35 +0100

    fixed compat with hybrid aquatic v1.5.4+

--- src/main/java/com/wdiscute/starcatcher/registry/fishing/compat/HybridAquaticFishingProperties.java
+++ src/main/java/com/wdiscute/starcatcher/registry/fishing/compat/HybridAquaticFishingProperties.java
@@ -292,8 +292,8 @@ public class HybridAquaticFishingProperties extends FishingPropertiesRegistry
                         .withWeather(FishProperties.Weather.CLEAR));
 
         register(
-                overworldDeepOceanFish(U.locItem("hybrid-aquatic", "sunfish"))
-                        .withEntityToSpawn(U.holderEntity("hybrid-aquatic", "sunfish"))
+                overworldDeepOceanFish(U.locItem("hybrid-aquatic", "ocean_sunfish"))
+                        .withEntityToSpawn(U.holderEntity("hybrid-aquatic", "ocean_sunfish"))
                         .withSizeAndWeight(FishProperties.sizeWeight(90, 10, 4300, 2500))
                         .withDifficulty(FishProperties.Difficulty.HARD_MOVING)
                         .withRarity(FishProperties.Rarity.RARE)
