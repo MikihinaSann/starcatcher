59247efa6f9c80c1e2d0432329326c4643a948c3
wd <lucasalvesbispo2@gmail.com>
2026-01-04 17:35:24 +0100

    work on display block and guide locking

--- src/main/java/com/wdiscute/starcatcher/registry/blocks/ModBlocks.java
+++ src/main/java/com/wdiscute/starcatcher/registry/blocks/ModBlocks.java
@@ -2,7 +2,6 @@ package com.wdiscute.starcatcher.registry.blocks;
 
 import com.wdiscute.starcatcher.Starcatcher;
 import com.wdiscute.starcatcher.registry.ModItems;
-import com.wdiscute.starcatcher.registry.blocks.Telescope.TelescopeBlock;
 import com.wdiscute.starcatcher.registry.blocks.display.DisplayBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -47,7 +46,7 @@ public interface ModBlocks
 
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block)
     {
-        ModItems.BLOCKITEMS_REGISTRY.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        ModItems.ITEMS_REGISTRY.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
     static void register(IEventBus eventBus)
