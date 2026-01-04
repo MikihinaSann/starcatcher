3fc17453660ad567731fe0227ec40f11916edbdb
wd <lucasalvesbispo2@gmail.com>
2026-01-04 17:10:53 +0000

    fixed all crab legs sweetspot particles not being the same color as the claw

--- src/main/java/com/wdiscute/starcatcher/storage/FishProperties.java
+++ src/main/java/com/wdiscute/starcatcher/storage/FishProperties.java
@@ -1333,7 +1333,7 @@ public record FishProperties(
 
         public static SweetSpot DEEPSLATE_CRAB_LEG = new SweetSpot(
                 ModSweetSpotsBehaviour.NORMAL,
-                RL_DEEPSLATE_CRAB_LEG, 15, 1, 0x310800
+                RL_DEEPSLATE_CRAB_LEG, 15, 1, 0xff8400
         );
 
         public static SweetSpot OBSIDIAN_CRAB_CLAW = new SweetSpot(
@@ -1343,7 +1343,7 @@ public record FishProperties(
 
         public static SweetSpot OBSIDIAN_CRAB_LEG = new SweetSpot(
                 ModSweetSpotsBehaviour.NORMAL,
-                RL_OBSIDIAN_CRAB_LEG, 15, 1, 0x1f1d30
+                RL_OBSIDIAN_CRAB_LEG, 15, 1, 0x3b2754
         );
 
         public static SweetSpot NETHER_CRAB_CLAW = new SweetSpot(
@@ -1353,7 +1353,7 @@ public record FishProperties(
 
         public static SweetSpot NETHER_CRAB_LEG = new SweetSpot(
                 ModSweetSpotsBehaviour.NORMAL,
-                RL_NETHER_CRAB_LEG, 15, 1, 0x330618
+                RL_NETHER_CRAB_LEG, 15, 1, 0xcd4545
         );
 
         public static SweetSpot END_CRAB_CLAW = new SweetSpot(
@@ -1363,7 +1363,7 @@ public record FishProperties(
 
         public static SweetSpot END_CRAB_LEG = new SweetSpot(
                 ModSweetSpotsBehaviour.NORMAL,
-                RL_END_CRAB_LEG, 15, 1, 0x10093f
+                RL_END_CRAB_LEG, 15, 1, 0xc67ed9
         );
 
 
