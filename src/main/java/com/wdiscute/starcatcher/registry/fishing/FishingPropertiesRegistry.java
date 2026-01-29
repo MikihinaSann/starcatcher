1b57a6ede5e347be961410145f35de0a9dbc89fe
wd <lucasalvesbispo2@gmail.com>
2026-01-29 01:57:56 +0100

    added Spawn compatibility, fixed lukewarm oceans restrictions, adjusted aqua sweetspot reward

--- src/main/java/com/wdiscute/starcatcher/registry/fishing/FishingPropertiesRegistry.java
+++ src/main/java/com/wdiscute/starcatcher/registry/fishing/FishingPropertiesRegistry.java
@@ -42,6 +42,7 @@ public class FishingPropertiesRegistry
         DGAquamiraeFishes.bootstrap();
         DGTerraFirmaCraftFishes.bootstrap();
         DGUnusualFishFishes.bootstrap();
+        DGSpawnFishes.bootstrap();
     }
 
     //region builders
