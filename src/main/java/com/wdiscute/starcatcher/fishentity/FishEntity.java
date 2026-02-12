e4bce592259f470490e2d9d184bb53570b417b55
wd <lucasalvesbispo2@gmail.com>
2026-02-12 12:25:01 +0000

    fixed fish entity crash, thank you redeix

--- src/main/java/com/wdiscute/starcatcher/fishentity/FishEntity.java
+++ src/main/java/com/wdiscute/starcatcher/fishentity/FishEntity.java
@@ -110,7 +110,7 @@ public class FishEntity extends AbstractFish
                 kill();
             else
             {
-                FishProperties fp = available.get(U.r.nextInt(available.size() - 1));
+                FishProperties fp = available.get(U.r.nextInt(available.size()));
                 ItemStack is = new ItemStack(fp.catchInfo().fish());
                 setFish(is);
             }
